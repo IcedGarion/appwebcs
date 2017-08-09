017a6d3d22fe53f7a74c04be94ce7ad09a569e7c
Garion <20010613@studenti.uniupo.it>
2017-08-09 14:50:06 +0200

    linq join

--- School/Controllers/HomeController.cs
+++ School/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using School.Model;
+using School.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace School.Controllers
 {
@@ -10,6 +14,21 @@ namespace School.Controllers
         //public IActionResult Index() => Redirect(Url.Action("Index", "Utente"));
 
         //passa subito alla view (Home/index)
-        public IActionResult Index() => View();
+        public IActionResult Index()
+        {
+            SchoolContext context = new SchoolContext();
+
+            //query separata dal suo controller per restituire elenco utenti (formattati nel loro datasource)
+            //perche' in View va specificato il @Model IEnumerable<UtentiDataSource>
+            //var query = from utenti in context.Utente select new UtentiDataSource { Username = utenti.Username, Password = utenti.Password };
+
+            var query = from ordini in context.Ordine
+                        join utenti in context.Utente on ordini.CdUtente equals utenti.CdUtente
+                        join ordineProdotto in context.OrdineProdotto on ordini.CdOrdine equals ordineProdotto.CdOrdine
+                        join prodotti in context.Prodotto on ordineProdotto.CdProdotto equals prodotti.CdProdotto
+                        select new OrdiniDataSource { CdOrdine = ordini.CdOrdine, Username = utenti.Username, Titolo = prodotti.Titolo, Totale = ordini.Totale };
+   
+            return View(query.ToList());
+        }
     }
 }
---
+++ School/Data/OrdiniDataSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Data
+{
+    public class OrdiniDataSource
+    {
+        public string Username { get; set; }
+
+        public int CdOrdine { get; set; }
+
+        public double Totale { get; set; }
+
+        public string Titolo { get; set; }
+    }
+}
